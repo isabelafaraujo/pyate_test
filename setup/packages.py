cd5fcec0d3fb8dd800b82845f8c1a75b87274204
Isabela Araújo <49255614+isabelafaraujo@users.noreply.github.com>
2020-09-18 01:15:57 -0300

    Update packages.py

--- setup/packages.py
+++ setup/packages.py
@@ -4,3 +4,4 @@
 !pip install https://github.com/explosion/spacy-models/releases/download/pt_core_news_sm-2.3.0/pt_core_news_sm-2.3.0.tar.gz
 !python -m spacy download en_core_web_sm
 !python -m spacy validate
+!python -m spacy download pt
